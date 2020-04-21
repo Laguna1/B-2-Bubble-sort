71417b333a500eaec86048eededebba8b8536e78
Laguna1 <lagunazoomarket@gmail.com>
2020-04-21 22:16:26 +0300

    add solution

---
+++ bubble_sort.rb
@@ -0,0 +1,17 @@
+def bubble_sort(my_array)
+    n = my_array.length
+swapped = true
+for i in 0...n-1 
+    swapped = false
+    for j in 0...n-i-1
+        if my_array[j] > my_array[j+1]
+            my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
+            swapped = true
+        end
+    end
+    break if swapped == false
+ end
+ return my_array
+end
+
+puts bubble_sort([4,3,78,2,0,2])
